39eacdab9ca893f2f13bb937a3cbdb111fc9ecfd
Paul ZC <paul.zimmerman.clayton@gmail.com>
2022-02-15 15:08:49 -0600

    Iterating over dummy static data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Passenger from './Passenger';
+import staticData from './StaticData.json';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passengers: staticData.data,
+    };
+  }
+
   render() {
-    return (
-      <div>
-        <Passenger />
-        <Passenger />
-        <Passenger />
-        <Passenger />
-        <Passenger />
-        <Passenger />
-        <Passenger />
-      </div>
-    );
+    const { passengers } = this.state;
+    console.log(passengers);
+    return <div className="" />;
   }
 }
 export default App;
