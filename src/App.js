4b4524911c079c2de4b6704a1e95838f0338d8db
Paul ZC <paul.zimmerman.clayton@gmail.com>
2022-02-15 21:54:33 -0600

    two hours is up

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends React.Component {
     super(props);
     this.state = {
       passengers: [],
+      loading: 'true',
+      // page: 0,
+      // prevY: 0,
     };
   }
 
@@ -17,23 +20,41 @@ class App extends React.Component {
   }
 
   getPassengers(page) {
+    this.setState({ loading: 'true' });
     axios
       .get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=50`)
       .then((res) => {
         this.setState({ passengers: res.data.data });
+        this.setState({ loading: 'false' });
       });
   }
 
   render() {
     const { passengers } = this.state;
 
+    const loadingCSS = {
+      height: '100px',
+      margin: '30px',
+    };
+
+    const loadingTextCSS = { display: this.state.loading ? 'block' : 'none' };
+
     return (
       <Container className="d-flex flex-wrap">
         {passengers.map((passenger) => (
           <Passenger key={passenger._id} passenger={passenger} />
         ))}
+        <div
+          ref={(loadingRef) => (this.loadingRef = loadingRef)}
+          style={loadingCSS}
+        >
+          <span style={loadingTextCSS}>Loading...</span>
+        </div>
       </Container>
     );
   }
 }
 export default App;
+
+// Previous commit loads, started to work through this:
+// https://www.pluralsight.com/guides/how-to-implement-infinite-scrolling-with-reactjs
