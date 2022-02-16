d7f7e8261cf5cb94a7be76c191c9eb66c8e0c20b
Paul ZC <paul.zimmerman.clayton@gmail.com>
2022-02-15 19:45:01 -0600

    Passenger proptypes, added bootstrap

--- src/Passenger.js
+++ src/Passenger.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Passenger extends React.Component {
   render() {
     const { passenger } = this.props;
-    return <div className="">{passenger.name}</div>;
+    const { _id, name, trips, airline, __v } = passenger;
+    return (
+      <div className="">
+        {name}, {trips}
+      </div>
+    );
   }
 }
+
+Passenger.propTypes = {
+  passenger: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    trips: PropTypes.number,
+    airline: {
+      id: PropTypes.number,
+      name: PropTypes.string,
+      country: PropTypes.string,
+      logo: PropTypes.string,
+      slogan: PropTypes.string,
+      head_quaters: PropTypes.string,
+      website: PropTypes.string,
+      established: PropTypes.string,
+    },
+    __v: PropTypes.number,
+  }),
+};
+
 export default Passenger;
