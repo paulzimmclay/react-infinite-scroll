2cacabffde5f095156fd294b0bf59ac041ed37b2
Paul ZC <paul.zimmerman.clayton@gmail.com>
2022-02-15 21:43:42 -0600

    getting data back from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import Passenger from './Passenger';
 import staticData from './StaticData.json';
 
@@ -11,7 +12,17 @@ class App extends React.Component {
     };
   }
 
-  getPassengers = () => {};
+  componentDidMount() {
+    this.getPassengers(3);
+  }
+
+  getPassengers(page) {
+    axios
+      .get(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=50`)
+      .then((res) => {
+        this.setState({ passengers: res.data.data });
+      });
+  }
 
   render() {
     const { passengers } = this.state;
